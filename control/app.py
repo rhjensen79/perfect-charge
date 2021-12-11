7374236c7f6f7fa0731c23d70a6d398d615dfeb4
Robert Jensen <robert@robert-jensen.dk>
2021-12-11 22:17:00 +0100

    working read json file

--- control/app.py
+++ control/app.py
@@ -1,7 +1,21 @@
 import os
 import time
+import json
 
 while __name__ == "__main__":
-    print ("Hello World From Control app - test")
 
-    time.sleep(10)
+    with open ("data/value.json") as jsonfile:
+        jsonObject = json.load(jsonfile)
+        result = jsonObject['result']
+        jsonfile.close()
+
+        for r in result:
+            value = (r['value'])
+            print ("Current price   : " + str(value))
+            start = (r['start'])
+            print ("Start Time      : " + str(start))
+            end = (r['end'])
+            print ("End Time        : " + str(end))
+
+
+    time.sleep(60)
