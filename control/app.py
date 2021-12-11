b13928a696b848575b3aa6646453299d16f9b139
Robert Jensen <robert@robert-jensen.dk>
2021-12-11 22:39:38 +0100

    added error handling when no file exist

--- control/app.py
+++ control/app.py
@@ -4,18 +4,21 @@ import json
 
 while __name__ == "__main__":
 
-    with open ("data/value.json") as jsonfile:
-        jsonObject = json.load(jsonfile)
-        result = jsonObject['result']
-        jsonfile.close()
+    try:
+        with open ("data/value.json") as jsonfile:
+            jsonObject = json.load(jsonfile)
+            result = jsonObject['result']
+            jsonfile.close()
 
-        for r in result:
-            value = (r['value'])
-            print ("Current price   : " + str(value))
-            start = (r['start'])
-            print ("Start Time      : " + str(start))
-            end = (r['end'])
-            print ("End Time        : " + str(end))
+            for r in result:
+                value = (r['value'])
+                print ("Current price   : " + str(value))
+                start = (r['start'])
+                print ("Start Time      : " + str(start))
+                end = (r['end'])
+                print ("End Time        : " + str(end))
+    except:
+        print ("File not found")
 
 
     time.sleep(60)
