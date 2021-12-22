ab2430dac74d9a4ae0a18bd89d9c07923c0db5fb
Robert Jensen <robert@robert-jensen.dk>
2021-12-22 14:55:58 +0100

    save monitoring app

--- monitoring/app.py
+++ monitoring/app.py
@@ -1,5 +1,31 @@
 import streamlit as st
+import json
+
+monitoringdata = {}
+monitoringdata['data'] = []
 
 st.title("Perfect Charge")
-st.write("This is the perfect chart monitoring/Control page")
+st.write("This is the perfect charge monitoring/Control page")
 
+try:
+    with open ("data/monitoring.json") as jsonfile:
+                jsonObject = json.load(jsonfile)
+                data = jsonObject['data']
+                jsonfile.close()
+                for d in data:
+                    chargevalue = (d['chargevalue'])
+
+except:
+    chargevalue = 3.0
+
+# Get Chargevalue input
+chargevalue = st.text_input("Charging will happen at this price or below : ",chargevalue)
+
+# Get all variables and save them in file
+
+monitoringdata['data'].append({
+    'chargevalue': chargevalue
+})
+with open("data/monitoring.json", "w") as f:
+    json.dump(monitoringdata, f)
+f.close()
