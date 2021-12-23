de74ef65062e92a16b325e640678bca5b7760661
Robert Jensen <robert@robert-jensen.dk>
2021-12-23 13:35:17 +0100

    saving monitoring - still problems with save, update value

--- monitoring/app.py
+++ monitoring/app.py
@@ -1,31 +1,39 @@
 import streamlit as st
 import json
+import logging
 
-monitoringdata = {}
-monitoringdata['data'] = []
 
-st.title("Perfect Charge")
-st.write("This is the perfect charge monitoring/Control page")
+if __name__ == "__main__":
+    st.title("Perfect Charge")
+    st.write("This is the perfect charge monitoring/Control page")
+      
 
-try:
+        
+
     with open ("data/monitoring.json") as jsonfile:
-                jsonObject = json.load(jsonfile)
-                data = jsonObject['data']
-                jsonfile.close()
-                for d in data:
-                    chargevalue = (d['chargevalue'])
-except:
-    chargevalue = 3.0
+        jsonObject = json.load(jsonfile)
+        data = jsonObject['data']
+        jsonfile.close()
+        for d in data:
+            chargevalue = (d['chargevalue'])
+            logging.info("--- Chargevalue recieved from file ---")
+            logging.info(chargevalue)
 
-print (chargevalue)
-# Get Chargevalue input
-chargevalue = st.number_input("Charging will happen at this price or below : ",chargevalue)
+    # Get Chargevalue input
+    chargevalue = st.number_input("Charging will happen at this price or below : ", chargevalue)
+    logging.info("--- New Chargevalue input recieved ---")
+    logging.info(chargevalue)
 
-# Get all variables and save them in file
 
-monitoringdata['data'].append({
-    'chargevalue': chargevalue
-})
-with open("data/monitoring.json", "w") as f:
-    json.dump(monitoringdata, f)
-f.close()
+    # Get all variables and save them in file
+    monitoringdata = {}
+    monitoringdata['data'] = []
+    #monitoringdata['data'].clear()
+    monitoringdata['data'].append({
+        'chargevalue': chargevalue
+    })
+    with open("data/monitoring.json", "w") as f:
+        json.dump(monitoringdata, f)
+    f.close()
+    logging.info("--- Saving Chargevalue ---")
+    logging.info(chargevalue)
