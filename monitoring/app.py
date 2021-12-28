c4842cc4910ca01a89a12a09663dde4ee37f88da
Robert Jensen <robert@robert-jensen.dk>
2021-12-28 20:54:01 +0100

    chaged app to function

--- monitoring/app.py
+++ monitoring/app.py
@@ -3,40 +3,50 @@ import json
 import logging
 
 
-st.title("Perfect Charge")
-st.write("This is the perfect charge monitoring/Control page")
+def main():
+    st.title("Perfect Charge")
+    st.write("This is the perfect charge monitoring/Control page")
     
+    
+    try:
+        with open ("data/monitoring.json") as jsonfile:
+            jsonObject = json.load(jsonfile)
+            data = jsonObject['data']
+            jsonfile.close()
+            for d in data:
+                chargevalue = (d['chargevalue'])
+                logging.info("--- Chargevalue recieved from file ---")
+                logging.info(chargevalue)
+    except:
+        with open("data/monitoring.json", "w") as f:
+            chargevalue = 3.0
+            f.close()
 
-    
-try:
-    with open ("data/monitoring.json") as jsonfile:
-        jsonObject = json.load(jsonfile)
-        data = jsonObject['data']
-        jsonfile.close()
-        for d in data:
-            chargevalue = (d['chargevalue'])
-            logging.info("--- Chargevalue recieved from file ---")
-            logging.info(chargevalue)
-except:
+    # Get Chargevalue input
+    chargevalue = st.number_input("Charging will happen at this price or below : ", chargevalue)
+    logging.info("--- New Chargevalue input recieved ---")
+    logging.info(chargevalue)
+
+
+    # Get all variables and save them in file
+    monitoringdata = {}
+    monitoringdata['data'] = []
+    #monitoringdata['data'].clear()
+    monitoringdata['data'].append({
+        'chargevalue': chargevalue
+    })
     with open("data/monitoring.json", "w") as f:
-        chargevalue = 3.0
-        f.close()
-
-# Get Chargevalue input
-chargevalue = st.number_input("Charging will happen at this price or below : ", chargevalue)
-logging.info("--- New Chargevalue input recieved ---")
-logging.info(chargevalue)
+        json.dump(monitoringdata, f)
+    f.close()
+    logging.info("--- Saving Chargevalue ---")
+    logging.info(chargevalue)
 
 
-# Get all variables and save them in file
-monitoringdata = {}
-monitoringdata['data'] = []
-#monitoringdata['data'].clear()
-monitoringdata['data'].append({
-    'chargevalue': chargevalue
-})
-with open("data/monitoring.json", "w") as f:
-    json.dump(monitoringdata, f)
-f.close()
-logging.info("--- Saving Chargevalue ---")
-logging.info(chargevalue)
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
